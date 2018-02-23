5c4e245f3a5c6f2eca2e77a404300559c0b8c994
Ezequiel <ezequiel.r.gallo@gmail.com>
2018-02-23 19:10:34 -0300

    instalando bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AbmProductosComponent } from './abm-productos/abm-productos.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AbmProductosComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
